4462b65d211aeb5e68532c76cef1e886d404d2bf
CinnaSpiceSystem <georaypolley@gmail.com>
2024-09-17 10:02:46 -0400

    Initial commit

---
+++ 8kyu/totalPoints.js
@@ -0,0 +1,39 @@
+// INFO
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+// Kata link: https://www.codewars.com/kata/5bb904724c47249b10000131
+
+
+
+
+const points = games => games.reduce((output, current) => {
+    // split the current game
+    let splitGame = current.split(":");
+
+    // convert matches to ints
+    let teamMatches = parseInt(splitGame[0]);
+    let opponentMatches = parseInt(splitGame[1]);
+
+    // get points to add depending on win, draw, or lose
+    let pointValue = teamMatches > opponentMatches ? 3 : teamMatches === opponentMatches ? 1 : 0;
+
+    // return sum of accumulator and point value of current match
+    return output + pointValue
+}, 0)
