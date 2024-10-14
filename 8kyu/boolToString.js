19ffc2967c831450e749011273faac95b07b36f1
nephpolley <georaypolley@gmail.com>
2024-10-14 13:53:19 -0400

    Convert Boolean to string

---
+++ 8kyu/boolToString.js
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Kata link: https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+function booleanToString(b){
+    return String(b)
+}
